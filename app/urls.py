37412627c52349eac25e34ff07f9f865bcd49df7
danielhinsching <danielsantoshinsching@gmail.com>
2024-07-09 14:39:31 -0300

    Criação da API "Acessório"

--- app/urls.py
+++ app/urls.py
@@ -10,7 +10,12 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from core.views import UserViewSet
+from core.views import UserViewSet
+from core.views import AcessorioViewSet 
 
+router = DefaultRouter()
+router.register(r"acessorios", AcessorioViewSet) 
+router.register(r"users", UserViewSet, basename="users")
 router = DefaultRouter()
 
 router.register(r"usuarios", UserViewSet, basename="usuarios")
