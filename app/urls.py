2a978b53e0d7aeb061ac2a4e2e6117009f859e0a
danielhinsching <danielsantoshinsching@gmail.com>
2024-07-09 14:48:55 -0300

    Criação da API "Categoria"

--- app/urls.py
+++ app/urls.py
@@ -11,10 +11,12 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from core.views import UserViewSet
 from core.views import UserViewSet
-from core.views import AcessorioViewSet 
+from core.views import AcessorioViewSet
+from core.views import CategoriaViewSet
 
 router = DefaultRouter()
-router.register(r"acessorios", AcessorioViewSet) 
+router.register(r"acessorios", AcessorioViewSet)
+router.register(r"categorias", CategoriaViewSet)
 router.register(r"users", UserViewSet, basename="users")
 router = DefaultRouter()
 
